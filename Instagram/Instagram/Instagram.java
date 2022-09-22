2fe8a05d08aa4d519da81c2e7692e634002a0791
yogeshwar0904 <110823632+yogeshwar0904@users.noreply.github.com>
2022-09-23 00:15:15 +0530

    Add files via upload

---
+++ Instagram/Instagram/Instagram.java
@@ -0,0 +1,16 @@
+import com.ideas2it.view.UserView;
+
+/**
+ * Call the User sugestion method to run the 
+ * application.
+ *
+ * @version     1.0 14 Sept 2022
+ * @author      Yogeshwar
+ */
+public class Instagram {
+    
+    public static void main(String[] args) { 
+        UserView userView = new UserView();
+        userView.getUserSugestion();
+    }
+}
